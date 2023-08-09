4383cd16513f5b007debb6c5b1383831a253a07b
mohankumar-git <mohankumar23@proton.me>
2023-08-09 18:43:19 +0000

    commit

---
+++ src/component/Game/StyledComponent.js
@@ -0,0 +1,114 @@
+import styled from 'styled-components'
+
+export const MainContainer = styled.div`
+  background-color: #223a5f;
+  height: 100vh;
+  padding: 30px;
+  color: #ffffff;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const ScoreContainer = styled.div`
+  padding: 20px;
+  border: 2px solid #ffffff;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-self: stretch;
+`
+
+export const ItemsContainer = styled.div``
+
+export const Heading = styled.h1`
+  font-family: 'Bree Serif';
+  font-size: 20px;
+  font-weight: 500;
+`
+
+export const ScoreCardContainer = styled.div`
+  margin: 15px;
+  background-color: #ffffff;
+  border-radius: 10px;
+  width: 100px;
+  padding: 5px;
+`
+
+export const ParagraphScore = styled.p`
+  font-family: 'Bree Serif';
+  font-weight: 500;
+  color: #000000;
+  font-size: 25px;
+  text-align: center;
+  margin: 0;
+`
+
+export const ScoreSpan = styled.p`
+  color: #000000;
+  text-align: center;
+  font-weight: bold;
+  font-size: 30px;
+  font-family: 'Roboto';
+  margin: 0;
+`
+
+export const ItemsImagesContainer = styled.ul`
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: space-around;
+  padding: 0;
+  width: 100%;
+  max-width: 700px;
+  height: 50%;
+`
+
+export const PlayAgainButton = styled.button`
+  border: 0;
+  background-color: #ffffff;
+  color: #000000;
+  font-family: 'Roboto';
+  font-size: 15px;
+  padding: 10px;
+  font-weight: bold;
+  border-radius: 10px;
+  cursor: pointer;
+`
+
+export const PopUpContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  align-self: stretch;
+`
+
+export const PopUpButton = styled(PlayAgainButton)`
+  width: 100px;
+  font-family: 'Bree Serif';
+  font-weight: 500;
+`
+
+export const RulesImageContainer = styled.div``
+
+export const RulesImage = styled.img`
+  width: 100%;
+  height: 100%;
+`
+export const CloseLineButton = styled.button`
+  border: 0;
+  background-color: #cdc9c9;
+  padding: 2px 4px;
+  border-radius: 4px;
+  cursor: pointer;
+  font-size: 25px;
+  margin-bottom: 6px;
+`
+
+export const CloseLineContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+`
